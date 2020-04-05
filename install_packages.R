c619fbb767b7b7a2b8d5027d2fdc057206afc88e
Martinez Rosales <diego.martinez@corteva.com>
2020-04-05 17:32:21 +0200

    RUN Rscript

--- install_packages.R
+++ install_packages.R
@@ -4,6 +4,8 @@ install.packages("emma")
 #Genomic tools intallation
 install.packages("BiocManager")
 library(BiocManager)
-install("snpStats", version = "3.8", ask = FALSE)
+install("snpStats", version = "3.5", ask = FALSE)
 library(snpStats)
 install("GenomicTools", ask = FALSE)
+    
+    
