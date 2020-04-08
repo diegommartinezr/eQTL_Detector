09c10a74eed8a6d0044933eb5e2e38110ad48ee3
Martinez Rosales <diego.martinez@corteva.com>
2020-04-09 00:15:55 +0200

    don't know commint

--- QTLtools.sh
+++ QTLtools.sh
@@ -5,5 +5,4 @@
 
 (cd home/rstudio/Data ; QTLtools pca --bed genes.50percent.chr22.bed.gz --scale --center --out genes.50percent.chr22)
 
-
 QTLtools pca --vcf genotypes.chr22.vcf.gz --scale --center --maf 0.05 --distance 50000 --out genotypes.chr22
