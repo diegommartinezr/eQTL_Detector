1bedfca87b54d1a69bc30d39fcd1c8c4c42f3897
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-04-10 00:36:18 +0200

    Update QTLtools_Test.sh

--- QTLtools_Test.sh
+++ QTLtools_Test.sh
@@ -20,25 +20,23 @@ QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --c
 QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --nominal 0.01 --region chr22:17000000-18000000 --out cis_nominal_N.txt --normal
 
 #[cis] to discover QTL in cis via a permutation pass of association
-
-
-QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --permute 1000 --region chr22:17000000-18000000 --out permutations.txt
-
-
-
-
+QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --permute 1000 --region chr22:17000000-18000000 --out cis_permutations.txt
+QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --permute 1000 --region chr22:17000000-18000000 --out cis_permutations_N.txt --normal --window 2000000
 
 #[cis] to discover multiple QTLs per phenotype in cis using a conditional pass
 
-#[trans] to discover QTL in trans using a full permutation pass
-QTLtools trans --vcf genotypes.chr22.vcf.gz --bed genes.simulated.chr22.bed.gz --nominal --threshold 1e-5 --out trans.nominal
-QTLtools trans --vcf genotypes.chr22.vcf.gz --bed genes.simulated.chr22.bed.gz --threshold 1e-5 --permute --out trans.perm123 --seed 123
 
-Rscript ~/script/plotTrans.R QQplot.pdf trans.nominal.hits.txt.gz trans.nominal.bins.txt.gz trans.perm123.hits.txt.gz trans.perm123.bins.txt.gz
+
+#[trans] to discover QTL in trans using a full permutation pass
+QTLtools trans --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --nominal --threshold 1e-5 --out out_trans.nominal 
 
 #[trans] to discover QTL in trans using an approximated permutation pass
+QTLtools trans --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --sample 1000 --normal --out out_trans.sample 
+##Check como obtener dicho script
 
 #[fdensity] to measure functional annotation density around QTLs
+
+
 
 #[fenrich] to measure enrichment of QTLs within annotations
 
@@ -63,9 +61,19 @@ mv out_pca.pca_stats /home/rstudio/Results
 #[cis] Nominal
 mv cis_nominal.txt /home/rstudio/Results
 mv cis_nominal_N.txt /home/rstudio/Results
+#[cis] Nominal permuation
+mv cis_permutations.txt /home/rstudio/Results
+mv cis_permutations_N.txt /home/rstudio/Results
+#[cis] conditional pass
 
-
-
+#[trans] conditional pass
+mv out_trans.nominal.best.txt.gz /home/rstudio/Results
+mv out_trans.nominal.bins.txt.gz /home/rstudio/Results
+mv out_trans.nominal.hits.txt.gz /home/rstudio/Results
+#[trans] permutation pass
+mv out_trans.sample.best.txt.gz /home/rstudio/Results
+mv out_trans.sample.bins.txt.gz /home/rstudio/Results
+mv out_trans.sample.hits.txt.gz /home/rstudio/Results
 
 
 
