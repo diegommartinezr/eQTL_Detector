3629011826a0fac2072607321e22328425fee339
Martinez Rosales <diego.martinez@corteva.com>
2020-04-09 13:34:42 +0200

    Exon output fiels

--- QTLtools_Test.sh
+++ QTLtools_Test.sh
@@ -7,6 +7,7 @@ cd /home/rstudio/Data
 #[bamstat] to control the quality of the sequence data
 QTLtools bamstat --bam HG00381.chr22.bam --bed gencode.v19.exon.chr22.bed.gz --filter-mapping-quality 150 --out HG00381.chr22.bamstat.txt
 mv HG00381.chr22.bamstat.txt /home/rstudio/Results
+mv HG00381.stats /home/rstudio/Results
 
 #[match] to ensure good matching between sequence and genotype data
 QTLtools mbv --bam HG00381.chr22.bam --vcf genotypes.chr22.vcf.gz --filter-mapping-quality 150 --out HG00381.chr22.bamstat.txt
@@ -14,11 +15,11 @@ mv HG00381.chr22.match.txt /home/rstudio/Results
 
 #[quan] to quantify gene expression
 QTLtools quan --bam HG00381.chr22.bam --gtf gencode.v19.annotation.chr22.gtf.gz --sample HG00381 --out-prefix HG00381 --filter-mapping-quality 150 --filter-mismatch 5 --filter-mismatch-total 5 --rpkm
-mv HG00381.5zDu4oPgLES.exon.count.bed /home/rstudio/Results
-mv HG00381.5zDu4oPgLES.exon.rpkm.bed /home/rstudio/Results
-mv HG00381.5zDu4oPgLES.gene.count.bed /home/rstudio/Results
-mv HG00381.5zDu4oPgLES.gene.rpkm.bed /home/rstudio/Results
-mv HG00381.5zDu4oPgLES.stats /home/rstudio/Results
+mv HG00381.exon.count.bed /home/rstudio/Results
+mv HG00381.exon.rpkm.bed /home/rstudio/Results
+mv HG00381.gene.count.bed /home/rstudio/Results
+mv HG00381.gene.rpkm.bed /home/rstudio/Results
+mv HG00381.stats /home/rstudio/Results
 
 #[pca] to perform PCA on phenotype and genotype data
 QTLtools pca --bed genes.50percent.chr22.bed.gz --scale --center --out genes.50percent.chr22
@@ -28,9 +29,11 @@ mv genes.50percent.chr22.pca_stats /home/rstudio/Results
 #[cis] to discover QTL in cis via a nominal pass of association
 QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --nominal 0.01 --region chr22:17000000-18000000 --out nominals.txt
 QTLtools cis --vcf genotypes.chr22.vcf.gz --bed genes.50percent.chr22.bed.gz --cov genes.covariates.pc50.txt.gz --nominal 0.01 --region chr22:17000000-18000000 --out nominals_withNormal.txt --normal
-
 mv nominals_withNormal.txt /home/rstudio/Results
 mv nominals.txt /home/rstudio/Results
+
+
+
 
 #[cis] to discover QTL in cis via a permutation pass of association
 
@@ -45,8 +48,6 @@ mv nominals.txt /home/rstudio/Results
 #[fenrich] to measure enrichment of QTLs within annotations
 
 #[rtc] to overlap QTLs with GWAS hits
-
-
 
 
 
