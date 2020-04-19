8a9c519aaaaa990515507ee30fc6036c51e59547
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-04-19 21:59:04 +0200

    Update quan.R

--- quan.R
+++ quan.R
@@ -1,22 +1,29 @@
-setwd("C:/Users/martindi/Desktop/eQTL_Detector")
+library(rlist)
 home <- getwd()
 quan <- file.path(home,"Results/quan")
 setwd(quan)
 list_quan <- list.files(path = quan)
 
-
 list_quan <- lapply(list_quan, function(x){
   read_delim(file = x,"\t", escape_double = FALSE, trim_ws = TRUE)})
-
 
 largo <- length(list_quan)
 vector_samples <- c(1:largo)
 
+list_all <- list.cbind(list_quan)
+
+RPKM <- data.frame(matrix(ncol = largo,
+                          nrow = length(list_quan[[1]]$start)))
+
+samples_names <- matrix(NA)
+
 for (a in vector_samples) {
-  sample_name <- colnames(list_quan[[a]][,7])
-  sample_quan <-data.frame(list_quan[[a]][,7])
-  base_matrix <- list_quan[[1]][,1:6]
-  RPKM <- data.frame(matrix(ncol = largo,
-                            nrow = length(base_matrix$start)))
-  RPKM[[a]] <- sample_quan[1]
-}
+  list_col <- colnames(list_all)
+  samples_names[a] <- list_col[a*7]
+  RPKM[,a] <- list_all[,a*7]
+  colnames(RPKM) <- samples_names
+  }
+
+RPKM <- cbind(list_quan[[1]][,1:6],RPKM)
+
+write.table(RPKM, "RPKM.bed")
