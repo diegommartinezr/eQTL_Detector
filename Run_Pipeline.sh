5a913d3af8f0b7584c007217c46f973b76992773
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-04-20 15:37:27 +0200

    Update Run_Pipeline.sh

--- Run_Pipeline.sh
+++ Run_Pipeline.sh
@@ -2,10 +2,5 @@
 
 #Docker-Compose installation
 
-mkdir Results
-mkdir Results/bamstat
-mkdir Results/mbv
-mkdir Results/quan
-
 docker-compose up -d
 docker exec eqtl_detector-master_server_1 /bin/sh /home/rstudio/QTLtools.sh
