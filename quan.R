fdd6d5069fee1581514c7b5d1e78f3269542284b
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-04-25 19:50:03 +0200

    Update quan.R

--- quan.R
+++ quan.R
@@ -20,24 +20,8 @@ list_all <- list.cbind(list_quan)
 RPKM <- data.frame(matrix(ncol = largo,
                           nrow = length(list_quan[[1]]$start)))
 
-samples_names <- matrix(NA)
 
-for (a in vector_samples) {
-  list_col <- colnames(list_all)
-  samples_names[a] <- list_col[a*7]
-  RPKM[,a] <- list_all[,a*7]
-  colnames(RPKM) <- samples_names
-}
-
-RPKM <- cbind(list_quan[[1]][,1:6],RPKM)
-
-BedRPKM <-convert2bed(RPKM,  set.type = FALSE,  
-                      check.zero.based = FALSE,  
-                      check.chr = FALSE,  
-                      check.valid = FALSE,  
-                      check.sort = FALSE,  
-                      check.merge = FALSE,  
-                      verbose = FALSE)
+RPKM <- list_quan[[1]][,1:6]
 
 
 write.table(BedRPKM, "RPKM.bed")
