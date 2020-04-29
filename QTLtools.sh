75e37c48b452cf7bf8d5ec937d438540543f48e4
Martinez Rosales <diego.martinez@corteva.com>
2020-04-29 13:42:25 +0200

    Update QTLtools.sh

--- QTLtools.sh
+++ QTLtools.sh
@@ -25,7 +25,7 @@ QTLtools bamstat \
   --bam $k \
   --bed gencode.v19.exon.chr22.bed.gz \
   --filter-mapping-quality 150 \
-  --out /home/rstudio/Results/bamstat$k.txt;
+  --out /home/rstudio/Results/bamstat/$k.txt;
 done
 
 
@@ -42,36 +42,27 @@ done
 
 
 
-
-
-
-
-
-
 #[quan] to quantify gene expression
 
 wget http://jungle.unige.ch/QTLtools_examples/gencode.v19.annotation.chr22.gtf.gz
 
 for k in *.bam;do
-QTLtools quan --bam $k --gtf gencode.v19.annotation.chr22.gtf.gz --sample "${k%.chr22.bam}" --out-prefix $k --filter-mapping-quality 150 --filter-mismatch 5 --filter-mismatch-total 5 --rpkm 
+QTLtools quan \
+  --bam $k \
+  --gtf gencode.v19.annotation.chr22.gtf.gz \
+  --sample "${k%.chr22.bam}" \
+  --out-prefix /home/rstudio/Results/quan/$k \
+  --filter-mapping-quality 150 \
+  --filter-mismatch 5 \
+  --filter-mismatch-total 5 \
+  --rpkm 
 done
 
-for r in *gene.rpkm.bed;do
-mv $r /home/rstudio/Results/quan
-done
-
-for r in *.bed;do
-mv $r /home/rstudio/Results/quan/stats
-done
-
-for r in *.stats;do
-mv $r /home/rstudio/Results/quan/stats
-done
 
 cd /home/rstudio/Results/quan
 
 
-FIRST=$(ls *rpkm.bed| head -1)
+FIRST=$(ls *.gene.rpkm.bed| head -1)
 
 cut -f 1-6 $FIRST >RPKM.bed
 
@@ -81,13 +72,29 @@ paste RPKM.bed RPKM_values.bed > RPKM_all.bed
 
 bgzip RPKM_all.bed  && tabix -p bed RPKM_all.bed.gz 
 
+
 #[pca]
 
-QTLtools pca --bed RPKM_all.bed.gz --scale --center --out /home/rstudio/Results/pca/RPKM.bed
+QTLtools pca \
+  --bed RPKM_all.bed.gz \
+  --scale \
+  --center \
+  --out /home/rstudio/Results/pca/RPKM.bed
 
-QTLtools pca --vcf GENOTYPES.vcf.gz --scale --center --maf 0.05 --distance 50000 --out genotypes_pca 
+
+QTLtools pca 
+  --vcf GENOTYPES.vcf.gz \
+  --scale \
+  --center \
+  --maf 0.05 \
+  --distance 50000 \
+  --out genotypes_pca 
+
+
 
 #[cis_nominal]
+
+
 ## Firts we get the data
 
 # Get the files for the cis analysiis
