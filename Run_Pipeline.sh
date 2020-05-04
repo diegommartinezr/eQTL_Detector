3e9a4d3478fef74bb999abe5eafdaea187d2af9e
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-05-04 22:57:43 +0200

    Update Run_Pipeline.sh

--- Run_Pipeline.sh
+++ Run_Pipeline.sh
@@ -1,3 +1,8 @@
+sudo apt install docker .io
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 docker build .
 docker-compose up -d
 VAR1=`docker ps | awk '{print $1}' | awk 'NR==2'`
