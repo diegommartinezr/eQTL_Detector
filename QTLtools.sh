4a2b84084ea94ee36b5f562fc7ddbb294a10a975
Diego Maximiliano Martínez Rosales <44356546+diegommartinezr@users.noreply.github.com>
2020-05-20 22:56:18 +0200

    Update QTLtools.sh

--- QTLtools.sh
+++ QTLtools.sh
@@ -141,28 +141,10 @@ QTLtools pca \
 
 #####################################################################################################################################
 #####################################################################################################################################
-#[cis]
+###############  cis eQTL  ##################
 
-
-cd /hone/rstudio/Bed-Seq
-
-
-# QTLtools cis \
-#  --vcf Genotypes.vcf.gz \
-#  --bed RPKM_all_sorted.bed.gz \
-#  --cov Cov.txt \
-#  --nominal 0.01 \
-#  --out /home/rstudio/Results/cis_nominal/nominals.txt
-
-
-# QTLtools cis \
-#  --vcf Genotypes.vcf.gz \
-#  --bed RPKM_all_sorted.bed.gz \
-#  --cov Cov.txt \
-#  --permute 1000 \
-#  --out /home/rstudio/Results/cis_nominal/permutation.txt
-
-####### usgin dummy data from the author
+#############################################
+#[cis] nominal
 
 cd /hone/rstudio/Bed-Seq
 
@@ -173,147 +155,54 @@ wget http://jungle.unige.ch/QTLtools_examples/genotypes.chr22.vcf.gz.tbi
 wget http://jungle.unige.ch/QTLtools_examples/genes.covariates.pc50.txt.gz
 
 
+# QTLtools cis \
+#  --vcf Genotypes.vcf.gz \
+#  --bed RPKM_all_sorted.bed.gz \
+#  --cov Cov.txt \
+#  --nominal 0.01 \
+#  --out /home/rstudio/Results/cis_nominal/nominals.txt
+
 QTLtools cis \
   --vcf genotypes.chr22.vcf.gz \
   --bed genes.50percent.chr22.bed.gz \
   --cov genes.covariates.pc50.txt.gz \
-  --nominal 0.0001 \
-  --region chr22:17000000-18000000 \
+  --nominal 0.00000001 \
   --out /home/rstudio/Results/cis_nominal/nominals.txt
+
+#############################################
+
+#[cis] permutation
+
+cd /home/rstudio/Results
+mkdir cis_permutation
+cd /hone/rstudio/Bed-Seq
+
+# QTLtools cis \
+#  --vcf Genotypes.vcf.gz \
+#  --bed RPKM_all_sorted.bed.gz \
+#  --cov Cov.txt \
+#  --permute 1000 \
+#  --out /home/rstudio/Results/cis_permutation/permutation.txt
+
+#using Expression pca
+
+#QTLtools cis \
+#--vcf Genotypes.vcf.gz \ 
+#--bed RPKM_all_sorted.bed.gz \ 
+#--cov Cov.txt \
+#--permute 1000 \ 
+#--grp-pca1 \ 
+#--out /home/rstudio/Results/cis_permutation/permutations.group.txt 
+
 
 QTLtools cis \
   --vcf genotypes.chr22.vcf.gz \
   --bed genes.50percent.chr22.bed.gz \
   --cov genes.covariates.pc50.txt.gz \
   --permute 1000 \
-  --region chr22:17000000-18000000 \
-  --out /home/rstudio/Results/cis_nominal/permutation.txt
-
-#####################################################################################################################################
-#####################################################################################################################################
-#[trans_full]
-
-#mkdir /home/rstudio/Results/trans_full
-
-#cd /hone/rstudio/Bed-Seq
-
-#QTLtools trans \
-#  --vcf Genotypes.vcf.gz \
-#  --bed RPKM_all_sorted.bed.gz \
-#  --nominal 0.0001 \
-#  --threshold 1e-5 \
-#  --out /home/rstudio/Results/trans/full.trans.nominal
+  --out /home/rstudio/Results/cis_permutation/permutation.txt
 
 
-#cd /hone/rstudio/Bed-Seq
-
-#QTLtools trans  \
-#  --vcf Genotypes.vcf.gz \ 
-#  --bed RPKM_all_sorted.bed.gz \
-#  --sample 1000 \
-#  --normal \ 
-#  --out full.trans.approx
-
-
-#QTLtools trans \ 
-#  --vcf Genotypes.vcf.gz \ 
-#  --bed RPKM_all_sorted.bed.gz \ 
-#  --adjust  \ .best.txt.gz \ 
-#  --normal --threshold 0.1 \ 
-#  --out trans.adjust
-
-####### usgin dummy data from the author
-
-cd /hone/rstudio/Bed-Seq
-
-wget http://jungle.unige.ch/QTLtools_examples/genotypes.chr22.vcf.gz
-wget http://jungle.unige.ch/QTLtools_examples/genotypes.chr22.vcf.gz.tbi
-
-
-wget http://jungle.unige.ch/QTLtools_examples/genes.simulated.chr22.bed.gz
-wget http://jungle.unige.ch/QTLtools_examples/genes.simulated.chr22.bed.gz.tbi
-
-
-QTLtools trans \
-  --vcf genotypes.chr22.vcf.gz \
-  --bed genes.simulated.chr22.bed.gz \
-  --nominal \
-  --threshold 1e-5 \
-  --out /home/rstudio/Results/trans/trans.nominal
-
-QTLtools trans \
-  --vcf genotypes.chr22.vcf.gz \
-  --bed genes.simulated.chr22.bed.gz \
-  --threshold 1e-5 \
-  --permute \
-  --out /home/rstudio/Results/trans/trans.perm123 \
-  --seed 123
-
-
-
-
-
-#####################################################################################################################################
-#####################################################################################################################################
-#####[fdensity]&[fenrich]
-
-cd /home/rstudio/Results/cis_nominal
-
-wget http://jungle.unige.ch/QTLtools_examples/results.genes.full.txt.gz
-
-#move the Rscripts
-
-for f in *.R;do
-mv /home/rstudio/Bed-Seq/$f /home/rstudio/Results/cis_nominal/$f;
-done
-
-mv /home/rstudio/Bed-Seq/TF.bed.gz /home/rstudio/Results/cis_nominal/TF.bed.gz
-
-#####[fdensity]
-
-#Rscript runFDR_cis.R permutation.txt 0.0001 results.permutation.genes 
-#cat results.permutation.genes.significant.txt | awk '{ print $9, $10-1, $11, $8, $1, $5 }' | tr " " "\t" | sort -k1,1 -k2,2n > permutation.results.genes.significant.bed 
-
-# QTLtools fdensity \
-# --qtl permutation.results.genes.significant.bed \
-# --bed TF.bed.gz \
-# --out nominal_density.TF.around.QTL.txt
-
-####[fenrich]
-
-#Rscript runFDR_cis.R permutation.txt 0.0001 results.genes
-#cat results.genes.significant.txt | awk '{ print $9, $10-1, $11, $8, $1, $5 }' | tr " " "\t" | sort -k1,1 -k2,2n > results.genes.significant.bed 
-#zcat results.genes.full.txt.gz | awk '{ print $2, $3-1, $4, $1, $8, $5 }' | tr " " "\t" | sort -k1,1 -k2,2n > results.genes.quantified.bed 
-
-#QTLtools fenrich \
-#  --qtl results.genes.significant.bed \
-#  --tss results.genes.quantified.bed \
-#  --bed TF.bed.gz \
-#  --out enrichement.QTL.in.TF.txt 
- 
-#Using dummyData
-
-#####[fdensity]
-
-Rscript runFDR_cis.R results.genes.full.txt.gz 0.0001 results.permutation.genes 
-cat results.permutation.genes.significant.txt | awk '{ print $9, $10-1, $11, $8, $1, $5 }' | tr " " "\t" | sort -k1,1 -k2,2n > permutation.results.genes.significant.bed 
-
- QTLtools fdensity \
- --qtl permutation.results.genes.significant.bed \
- --bed TF.bed.gz \
- --out nominal_density.TF.around.QTL.txt
-
-####[fenrich]
-
-zcat results.genes.full.txt.gz | awk '{ print $2, $3-1, $4, $1, $8, $5 }' | tr " " "\t" | sort -k1,1 -k2,2n > results.genes.quantified.bed 
-
-QTLtools fenrich \
-  --qtl results.genes.significant.bed \
-  --tss results.genes.quantified.bed \
-  --bed TFs.encode.bed.gz \
-  --out enrichement.QTL.in.TF.txt
-  
- 
 
 #####################################################################################################################################
 #####################################################################################################################################
